d76d1c4c95d69d020cc790174b474ae31e493990
komiakov <andrii.komiakov@icloud.com>
2025-01-22 20:12:23 +0200

    migrate to nuxt.js

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        <NavMenu></NavMenu>
+        <NuxtPage />
+    </div>
+</template>
+
+<style lang="scss" >
+    #app{
+        width: 100vw; height: 100vh;
+        overflow: hidden; white-space: nowrap;
+    }
+</style>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  css: ['~/assets/reset.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/colors.scss" as *;'
+        }
+      }
+    }
+  },
+
+  modules: ['@nuxt/ui']
+})
